63c045624cc73a4bbce83e558c9ddcb4bba225ed
Ben Naylor <ben.naylor89@gmail.com>
2020-06-12 21:44:05 +0100

    Created price data model, began writing to db

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,16 @@
 const pricing = require('pricing')
 const database = require('database')
 const util = require('util')
+const Price = require('../model/price')
 
 const setTimeoutPromise = util.promisify(setTimeout)
 pricing.setCurrency('BTC-GBP')
 
 const mainLoop = async () => {
-  const buyPrice = await pricing.getBuyPrice();
-  const sellPrice = await pricing.getSellPrice();
-  const spotPrice = await pricing.getSpotPrice();
 
-  console.log(`Buy: ${buyPrice.amount}`)
-  console.log(`Sell: ${sellPrice.amount}`)
-  console.log(`Spot: ${spotPrice.amount}`)
+  const prices = await pricing.getPrices();
+  const price = await Price.create(prices)
+  console.log(prices)
 
   const time = 5 * 1000
   await setTimeoutPromise(time)
@@ -29,4 +27,5 @@ module.exports = {
 
   }
 }
+
 
