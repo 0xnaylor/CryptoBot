fe930cfd3bb41b1cc80e02f0bd03fbdbd3e508c5
Ben Naylor <ben.naylor89@gmail.com>
2020-06-17 09:46:16 +0100

    Added mean and median functionality

--- src/app/index.js
+++ src/app/index.js
@@ -2,29 +2,29 @@ const pricing = require('pricing')
 const database = require('database')
 const util = require('util')
 const Price = require('../model/price')
+const Trading = require('../trading')
 
 const setTimeoutPromise = util.promisify(setTimeout)
-pricing.setCurrency('BTC-GBP')
 
 const mainLoop = async () => {
-
-  const prices = await pricing.getPrices();
-  const price = await Price.create(prices)
-  console.log(prices)
-
-  const time = 5 * 1000
-  await setTimeoutPromise(time)
-  mainLoop()
+  const time = 10 * 1000
+  try {
+    const prices = await pricing.getPrices();
+    const price = await Price.create(prices)
+    console.log("Price obtained")
+    await Trading.onPrices(price)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 module.exports = {
 
-
   start: async () => {
     
     await database.connect()
+    setInterval(mainLoop, 10 * 1000)
     mainLoop()
-
   }
 }
 
